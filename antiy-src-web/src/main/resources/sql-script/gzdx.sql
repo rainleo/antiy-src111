1e91ca2b6f79d1033dfb36acdeacd191dad07884
lvliang <1115653400@qq.com>
2020-02-19 14:42:40 +0800

    feat: 漏洞模块

--- antiy-src-web/src/main/resources/sql-script/gzdx.sql
+++ antiy-src-web/src/main/resources/sql-script/gzdx.sql
@@ -11,9 +11,8 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 18/02/2020 09:40:17
+ Date: 19/02/2020 14:29:39
 */
-
 CREATE  database
 IF NOT  EXISTS  `gzdx` DEFAULT  CHARACTER
 SET utf8mb4 COLLATE  utf8mb4_unicode_ci;
@@ -73,7 +72,7 @@ CREATE TABLE `event_level`  (
   `event_level` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '事件等级',
   `content` varchar(600) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '事件描述',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of event_level
@@ -116,7 +115,7 @@ CREATE TABLE `menu`  (
   `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '菜单标识',
   `type` tinyint(1) DEFAULT NULL COMMENT '菜单类型 1、菜单 2、按钮',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of menu
@@ -151,7 +150,7 @@ CREATE TABLE `role`  (
   `tag` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '角色标识',
   `description` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of role
@@ -174,29 +173,56 @@ CREATE TABLE `role_menu`  (
 -- ----------------------------
 -- Records of role_menu
 -- ----------------------------
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (1, 2);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 3);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 4);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 7);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 10);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 11);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 18);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 19);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 20);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (2, 21);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (3, 7);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (3, 10);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (3, 12);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (3, 18);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (3, 20);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (4, 7);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (4, 8);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (4, 10);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (4, 15);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (4, 16);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (4, 18);
-INSERT INTO `role_menu`(`role_id`, `menu_id`) VALUES (4, 20);
-
+INSERT INTO `role_menu` VALUES (1, 1);
+INSERT INTO `role_menu` VALUES (1, 2);
+INSERT INTO `role_menu` VALUES (1, 3);
+INSERT INTO `role_menu` VALUES (1, 4);
+INSERT INTO `role_menu` VALUES (1, 5);
+INSERT INTO `role_menu` VALUES (1, 7);
+INSERT INTO `role_menu` VALUES (1, 8);
+INSERT INTO `role_menu` VALUES (1, 10);
+INSERT INTO `role_menu` VALUES (1, 11);
+INSERT INTO `role_menu` VALUES (1, 12);
+INSERT INTO `role_menu` VALUES (1, 13);
+INSERT INTO `role_menu` VALUES (1, 14);
+INSERT INTO `role_menu` VALUES (1, 15);
+INSERT INTO `role_menu` VALUES (1, 16);
+INSERT INTO `role_menu` VALUES (1, 17);
+INSERT INTO `role_menu` VALUES (1, 18);
+INSERT INTO `role_menu` VALUES (1, 19);
+INSERT INTO `role_menu` VALUES (1, 20);
+INSERT INTO `role_menu` VALUES (1, 21);
+INSERT INTO `role_menu` VALUES (2, 1);
+INSERT INTO `role_menu` VALUES (2, 2);
+INSERT INTO `role_menu` VALUES (2, 3);
+INSERT INTO `role_menu` VALUES (2, 4);
+INSERT INTO `role_menu` VALUES (2, 5);
+INSERT INTO `role_menu` VALUES (2, 7);
+INSERT INTO `role_menu` VALUES (2, 8);
+INSERT INTO `role_menu` VALUES (2, 10);
+INSERT INTO `role_menu` VALUES (2, 11);
+INSERT INTO `role_menu` VALUES (2, 12);
+INSERT INTO `role_menu` VALUES (2, 13);
+INSERT INTO `role_menu` VALUES (2, 14);
+INSERT INTO `role_menu` VALUES (2, 15);
+INSERT INTO `role_menu` VALUES (2, 16);
+INSERT INTO `role_menu` VALUES (2, 17);
+INSERT INTO `role_menu` VALUES (2, 18);
+INSERT INTO `role_menu` VALUES (2, 19);
+INSERT INTO `role_menu` VALUES (2, 20);
+INSERT INTO `role_menu` VALUES (2, 21);
+INSERT INTO `role_menu` VALUES (3, 7);
+INSERT INTO `role_menu` VALUES (3, 10);
+INSERT INTO `role_menu` VALUES (3, 12);
+INSERT INTO `role_menu` VALUES (3, 18);
+INSERT INTO `role_menu` VALUES (3, 20);
+INSERT INTO `role_menu` VALUES (4, 7);
+INSERT INTO `role_menu` VALUES (4, 8);
+INSERT INTO `role_menu` VALUES (4, 10);
+INSERT INTO `role_menu` VALUES (4, 15);
+INSERT INTO `role_menu` VALUES (4, 16);
+INSERT INTO `role_menu` VALUES (4, 18);
+INSERT INTO `role_menu` VALUES (4, 20);
 
 -- ----------------------------
 -- Table structure for task_info
@@ -205,7 +231,7 @@ DROP TABLE IF EXISTS `task_info`;
 CREATE TABLE `task_info`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '任务名称',
-  `task_type` int(11) DEFAULT NULL COMMENT '1.排雷2.扫雷',
+  `task_type` int(11) DEFAULT NULL COMMENT '1.扫雷2.排雷',
   `task_no` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '任务编号',
   `start_time` bigint(20) NOT NULL COMMENT '开始时间',
   `end_time` bigint(20) NOT NULL COMMENT '结束时间',
@@ -216,7 +242,7 @@ CREATE TABLE `task_info`  (
   `gmt_modify` bigint(20) DEFAULT NULL,
   `status` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 51 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of task_info
@@ -244,12 +270,12 @@ CREATE TABLE `user`  (
   `modified_user` int(11) DEFAULT NULL COMMENT '修改用户',
   `gmt_modified` bigint(20) DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 35 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 674713460738621440, 'admin', 'qBWzqgUZxbZ4bCLwsQA2Sw==', '刘三', 1, '510823199107147030', '18789092987', 1, NULL, 0, 1581931046915, 0, 1580905824610, NULL, NULL);
+INSERT INTO `user` VALUES (1, 674713460738621440, 'admin', 'qBWzqgUZxbZ4bCLwsQA2Sw==', '刘三', 1, '510823199107147030', '18789092987', 1, NULL, 0, 1582093693209, 0, 1580905824610, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for user_role
@@ -265,27 +291,7 @@ CREATE TABLE `user_role`  (
 -- Records of user_role
 -- ----------------------------
 INSERT INTO `user_role` VALUES (674713460738621440, 1);
-INSERT INTO `user_role` VALUES (677581395559710720, 4);
-INSERT INTO `user_role` VALUES (677623148866174976, 4);
-INSERT INTO `user_role` VALUES (677823457165049856, 4);
-INSERT INTO `user_role` VALUES (677869054702125056, 4);
-INSERT INTO `user_role` VALUES (677870377233285120, 4);
-INSERT INTO `user_role` VALUES (677870544044949504, 4);
-INSERT INTO `user_role` VALUES (677902600011513856, 2);
-INSERT INTO `user_role` VALUES (677940817368711168, 2);
-INSERT INTO `user_role` VALUES (677949304924536832, 4);
-INSERT INTO `user_role` VALUES (677951315669680128, 2);
-INSERT INTO `user_role` VALUES (678914128978378752, 4);
-INSERT INTO `user_role` VALUES (678916900561879040, 3);
-INSERT INTO `user_role` VALUES (678920707152805888, 4);
-INSERT INTO `user_role` VALUES (678927166217388032, 2);
-INSERT INTO `user_role` VALUES (678927470698692608, 3);
-INSERT INTO `user_role` VALUES (678929798956843008, 4);
-INSERT INTO `user_role` VALUES (678971557057396736, 4);
-INSERT INTO `user_role` VALUES (678978486903242752, 4);
-INSERT INTO `user_role` VALUES (679000352778878976, 3);
-INSERT INTO `user_role` VALUES (679013847146692608, 2);
-INSERT INTO `user_role` VALUES (679018398482432000, 3);
+
 
 -- ----------------------------
 -- Table structure for vul_examine_info
@@ -296,12 +302,12 @@ CREATE TABLE `vul_examine_info`  (
   `vul_id` int(11) NOT NULL COMMENT '漏洞id',
   `examiner` bigint(20) NOT NULL COMMENT '审批人',
   `result` int(11) NOT NULL COMMENT '2通过3未通过',
-  `reason` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '审批理由',
-  `examine_material_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '审批材料名',
-  `examine_material_url` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '审批材料url',
+  `reason` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '审批理由',
+  `examine_material_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '审批材料名',
+  `examine_material_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '审批材料url',
   `examine_date` bigint(20) DEFAULT NULL COMMENT '审批日期',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 26 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of vul_examine_info
@@ -325,8 +331,8 @@ CREATE TABLE `vul_info`  (
   `vul_level` tinyint(4) NOT NULL COMMENT '1高危漏洞，2中危漏洞3.低危漏洞',
   `event_level` varchar(8) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'A,B,C,D拼接',
   `event_level_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '事件等级详细',
-  `vul_desc` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '漏洞描述',
-  `repair_suggest` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '修复建议',
+  `vul_desc` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '漏洞描述',
+  `repair_suggest` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '修复建议',
   `vul_material_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '漏洞材料名称',
   `vul_material_url` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '漏洞材料url',
   `vul_status` tinyint(4) DEFAULT NULL COMMENT '1待审核2通过3未通过',
@@ -338,11 +344,12 @@ CREATE TABLE `vul_info`  (
   `gmt_modify` bigint(20) DEFAULT NULL,
   `status` tinyint(4) DEFAULT 1 COMMENT '0删除1正常',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 26 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of vul_info
 -- ----------------------------
+
 -- ----------------------------
 -- Table structure for vul_integral_info
 -- ----------------------------
@@ -360,7 +367,7 @@ CREATE TABLE `vul_integral_info`  (
   `commit_user` bigint(20) NOT NULL COMMENT '提交人',
   `department` int(11) NOT NULL COMMENT '所属部门',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB  CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of vul_integral_info

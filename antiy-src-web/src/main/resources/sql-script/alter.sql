f95886ab9dea8ebef221d114b0e233e77259e770
wangqian <wangqian@antiy.cn>
2020-03-04 16:53:10 +0800

    feat: 增量脚本

--- antiy-src-web/src/main/resources/sql-script/alter.sql
+++ antiy-src-web/src/main/resources/sql-script/alter.sql
@@ -1,2 +1,3 @@
 use gzdx;
 alter table user modify column phone varchar(30);
+update user set idcard = '2xheJ5d0TktHVVtlzYwvb3UOA/Y/psLt9Cc3Voi9Fas=', phone = 'f88yeDoaTof3YQGBz+GbLA==' where username = 'admin';

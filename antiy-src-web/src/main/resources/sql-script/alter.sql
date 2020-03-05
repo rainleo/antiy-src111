5032bdd6105032f547b54cdb9b072b260d17d9d6
wangqian <wangqian@antiy.cn>
2020-03-05 10:27:04 +0800

    feat: 字段值校正

--- antiy-src-web/src/main/resources/sql-script/alter.sql
+++ antiy-src-web/src/main/resources/sql-script/alter.sql
@@ -1,2 +1,3 @@
 use gzdx;
 alter table user modify column phone varchar(200);
+update user set idcard = to_base64(AES_ENCRYPT(idcard,'abcdefgabcdefg12')), phone = to_base64(AES_ENCRYPT(phone,'abcdefgabcdefg12'));
